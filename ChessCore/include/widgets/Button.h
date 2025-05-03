e3ae76e939b1a49f3c20534404c53ea0f00fbcaa
zendeswaraj53@gmail.com <zendeswaraj53@gmail.com>
2025-05-03 16:39:34 +0530

    made the functoins override

--- ChessCore/include/widgets/Button.h
+++ ChessCore/include/widgets/Button.h
@@ -30,24 +30,24 @@ namespace chess
 
             Delegate<> mOnButtonClicked;
         private:
-        virtual void Draw(sf::RenderWindow& windowRef);
-        virtual void LocationUpdated(const sf::Vector2f& newLocation);
-        virtual void RotationUpdated(const sf::Angle& newRotation);
-
-        void CenterText();
-
-        shared<sf::Texture> mButtonTexture;
-        sf::Sprite mButtonSprite;
-            
-        shared<sf::Font> mButtonFont;
-        sf::Text mButtonText;
-
-        ButtonColor mButtonColor;
-
-        bool mIsButtonDown;
-
-        void ButtonUp();
-        void ButtonDown();
-        void MouseHovered();
+            virtual void Draw(sf::RenderWindow& windowRef)override;
+            virtual void LocationUpdated(const sf::Vector2f& newLocation)override;
+            virtual void RotationUpdated(const sf::Angle& newRotation)override;
+        
+            void CenterText();
+        
+            shared<sf::Texture> mButtonTexture;
+            sf::Sprite mButtonSprite;
+                
+            shared<sf::Font> mButtonFont;
+            sf::Text mButtonText;
+        
+            ButtonColor mButtonColor;
+        
+            bool mIsButtonDown;
+        
+            void ButtonUp();
+            void ButtonDown();
+            void MouseHovered();
     };
 }
